0870c4e674ebffb740f7d516b7797a4c1e9c5575
堤　竜也 <tsuttsuuun@gmail.com>
2020-12-31 12:44:20 +0900

    Task. 5-3 テスト環境の構築まで完了

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,20 @@
-require_relative 'boot'
+require_relative "boot"
+
+config.generators do |g|
+  g.template_engine false
+  g.javascripts false
+  g.stylesheets false
+  g.helper false
+  g.test_framework :rspec,
+                   view_specs: false,
+                   routing_specs: false,
+                   helper_specs: false,
+                   controller_specs: false,
+                   request_specs: true
+end
+
+config.api_only = true
+
 
 require "rails"
 # Pick the frameworks you want:
