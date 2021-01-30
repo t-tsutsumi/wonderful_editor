0682b31b9cd75d2d0a8c5a39c8b5c7c279a1eaed
堤　竜也 <tsuttsuuun@gmail.com>
2021-01-30 11:20:47 +0900

    Task. 8「フロントエンドとのつなぎ込み」まで終了

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,19 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+  <v-app id="app">
+    <v-container>
+      <router-view></router-view>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
     }
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
 </style>
--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -28,7 +28,6 @@ RSpec.describe "Api::V1::Articles", type: :request do
       let(:article) { create(:article) }
       let(:article_id) { article.id }
 
-
       it "任意の記事の値が取得できる" do
         subject
         res = JSON.parse(response.body)
@@ -42,9 +41,6 @@ RSpec.describe "Api::V1::Articles", type: :request do
         expect(res["user"].keys).to eq ["id", "name", "email"]
       end
     end
-
-    # rubocop:enable all
-
     context "指定した id の記事が存在しない場合" do
       let(:article_id) { 10000 }
 
@@ -53,4 +49,84 @@ RSpec.describe "Api::V1::Articles", type: :request do
       end
     end
   end
+
+
+  describe "POST /articles" do
+    subject { post(api_v1_articles_path, params: params) }
+
+    let(:params) { { article: attributes_for(:article) } }
+    let(:current_user) { create(:user) }
+
+    # stub
+    before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+
+    it "記事のレコードが作成できる" do
+      expect { subject }.to change { Article.where(user_id: current_user.id).count }.by(1)
+      res = JSON.parse(response.body)
+      expect(res["title"]).to eq params[:article][:title]
+      expect(res["body"]).to eq params[:article][:body]
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "PATCH /api/v1/articles/:id" do
+    subject { patch(api_v1_article_path(article.id), params: params) }
+
+    let(:params) { { article: attributes_for(:article) } }
+    let(:current_user) { create(:user) }
+    before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+
+    context "自分が所持している記事のレコードを更新しようとするとき" do
+      let(:article) { create(:article, user: current_user) }
+
+      it "記事を更新できる" do
+        expect { subject }.to change { article.reload.title }.from(article.title).to(params[:article][:title]) &
+                              change { article.reload.body }.from(article.body).to(params[:article][:body])
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "自分が所持していない記事のレコードを更新しようとするとき" do
+      let(:other_user) { create(:user) }
+      let!(:article) { create(:article, user: other_user) }
+
+      it "更新できない" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound) &
+                              change { Article.count }.by(0)
+      end
+    end
+  end
+
+  describe "DELETE /api/v1/articles/:id" do
+    subject { delete(api_v1_article_path(article.id)) }
+
+    # devise_token_auth の導入が完了後に削除
+    let(:current_user) { create(:user) }
+    let(:article_id) { article.id }
+    before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+
+    # it "任意のユーザーのレコードを削除できる" do
+    #   expect { subject }.to change { Article.count }.by(-1)
+
+      context "自分の記事を削除しようとするとき" do
+        let!(:article) { create(:article, user: current_user) }
+
+        it "記事を削除できる" do
+          expect { subject }.to change { Article.count }.by(-1)
+          expect(response).to have_http_status(:no_content)
+        end
+      end
+
+      context "他人が所持している記事のレコードを削除しようとするとき" do
+        let(:other_user) { create(:user) }
+        let!(:article) { create(:article, user: other_user) }
+
+        it "記事を削除できない" do
+          expect { subject }.to raise_error(ActiveRecord::RecordNotFound) &
+                                change { Article.count }.by(0)
+        end
+      end
+
+  end
 end
+# rubocop:enable all
